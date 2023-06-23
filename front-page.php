d661e774a3b46c41aa95998d9d6f1eeadb6e02d5
Benjamin Picquet <benjamin.picquet@hotmail.com>
2023-06-23 11:47:47 +0200

    add subheader section in frontpage

--- front-page.php
+++ front-page.php
@@ -1,3 +1,21 @@
 <?php get_header(); ?>
-	<p>dsffsd</p>
+
+<section id="frontpage-subheader" class="frontpage-subheader">
+    <div class="frontpage-subheader__container">
+		<video
+			class="frontpage-subheader__container__video"
+			muted
+			autoplay>
+				<source src="<?= get_template_directory_uri() . '/assets/img/colivein-hero-video.mp4' ?>" type="video/mp4" />
+		</video>
+		<div>
+			<div class="container">
+				<h1>Bienvenue dans nos maisons partagées pour séniors autonomes</h1>
+				<p>Chez Colive’in on vit en parfaite autonomie dans des maisons familiales typiques de leurs régions, proche de toutes commodités (pharmacie, transport en commun…) <strong>regroupant 4 à 6 personnes</strong> par affinités souhaitant partager leur retraite sous le signe de la convivialité !</p>
+				<p>Nous sommes convaincus que les relations humaines et le maintien de l’autonomie sont le ciment d’une retraite épanouie et dynamique.</p>
+				<p>Ensemble, vivons une seconde jeunesse ?</p>
+			</div>
+		</div>
+	</div>
+</section>
 <?php get_footer(); ?>
