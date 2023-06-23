2ea8f2bd23803aa73fe3442e1e9c1e2c4b15e255
Benjamin Picquet <benjamin.picquet@hotmail.com>
2023-06-23 15:47:02 +0200

    add advantage section in frontpage

--- front-page.php
+++ front-page.php
@@ -19,4 +19,52 @@
 	</div>
 </section>
 
+<section id="frontpage-listing-advantage" class="frontpage-listing-advantage">
+	<div class="frontpage-listing-advantage__container container">
+		<h2>Des maisons pensées pour vous</h2>
+		<p>Nos maisons disposent de grands espaces communs et privatifs conçu pour <strong>préserver l’autonomie.</strong> Cette ambiance sereine et confortable vous permettra de passer de beaux moments d’échange entre amis </p>
+		<div class="frontpage-listing-advantage__container__card-wrapper">
+			<div class="frontpage-listing-advantage__container__card-wrapper__card">
+				<img src="https://colivein.com/wp-content/uploads/2022/11/AdobeStock_193059911-1.jpeg" alt="">
+				<div>
+					<p>Sécurité</p>
+				</div>
+			</div>
+			<div class="frontpage-listing-advantage__container__card-wrapper__card">
+				<img src="https://colivein.com/wp-content/uploads/2022/11/AdobeStock_142559683-1536x1025.jpeg" alt="">
+				<div>
+					<p>Chambre privative ou partagée</p>
+				</div>
+			</div>
+			<div class="frontpage-listing-advantage__container__card-wrapper__card">
+				<img src="https://colivein.com/wp-content/uploads/2022/11/AdobeStock_358627891.jpeg" alt="">
+				<div>
+					<p>Bien-être au quotidien</p>
+				</div>
+			</div>
+			<div class="frontpage-listing-advantage__container__card-wrapper__card">
+				<img src="https://colivein.com/wp-content/uploads/2022/11/AdobeStock_509401611-1-1536x1024.jpg" alt="">
+				<div>
+					<p>Services inclus</p>
+				</div>
+			</div>
+			<div class="frontpage-listing-advantage__container__card-wrapper__card">
+				<img src="https://colivein.com/wp-content/uploads/2022/11/AdobeStock_410079959.jpeg" alt="">
+				<div>
+					<p>Cuisine de qualité</p>
+				</div>
+			</div>
+			<div class="frontpage-listing-advantage__container__card-wrapper__card">
+				<img src="https://colivein.com/wp-content/uploads/2022/11/AdobeStock_296426930-1536x1025.jpeg" alt="">
+				<div>
+					<p>Maison connectée</p>
+				</div>
+			</div>
+		</div>
+		<a href="">
+			<button>En savoir plus <i class="fa-sharp fa-solid fa-right-long fa-sm"></i></button>
+		</a>
+	</div>
+</section>
+
 <?php get_footer(); ?>
