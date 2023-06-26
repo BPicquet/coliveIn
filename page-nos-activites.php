1040767537924ac3e88f8fbd10d7b977d8a3f54d
Benjamin Picquet <benjamin.picquet@hotmail.com>
2023-06-26 17:56:38 +0200

    add our-activity page integration

---
+++ page-nos-activites.php
@@ -0,0 +1,87 @@
+<?php get_header(); ?>
+
+<section id="our-activity-subheader" class="our-activity-subheader">
+    <div class="our-activity-subheader__container">
+        <img src="<?= get_template_directory_uri() . '/assets/img/nos-activites-subheader.png' ?>" alt="">
+        <div>
+            <div class="container">
+                <h2>Les activités : chez Colive'in tout est possible !</h2>
+                <p><strong>« Vieillir ensemble ce n’est pas rajouter des années à sa vie, mais rajouter de la vie à ses années » JFK</strong></p>
+                <p>Nos partenaires vont faire de votre quotidien <strong>un moment de partage et de découverte </strong> au travers de nos multitudes d’activités.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="our-activity-live-together" class="our-activity-live-together">
+    <div class="our-activity-live-together__container container">
+        <h2>"Vivre ensemble"</h2>
+        <p>
+            Certains aimeront <strong> faire des balades, marcher, découvrir le patrimoine de la région, voyager, jouer aux jeux de société, aller au cinéma… </strong>
+            </br>Autant de moment pour se réunir et motiver les autres colocataires à partir à la découverte de nouvelles activités.
+        </p>
+        <p><strong>Profitez tout en économisant !</strong></p>
+        <p>Grâce à nos nombreux partenariats nous vous offrons la possibilité de mutualiser les coûts sur l’ensemble des activités proposées.</p>
+        <div class="our-activity-live-together__container__card-wrapper">
+            <div class="our-activity-live-together__container__card-wrapper__card">
+                <div>
+                    <i class="fa-solid fa-heart"></i>
+                    <div>
+                        <h3>Bien-être</h3>
+                        <p>Yoga, relaxation, expression corporelle...</p>
+                    </div>
+                </div>
+                <img src="<?= get_template_directory_uri() . '/assets/img/nos-activites-bien-etre.jpeg' ?>" alt="">
+            </div>
+            <div class="our-activity-live-together__container__card-wrapper__card">
+                <div>
+                    <i class="fa-solid fa-heart-pulse"></i>
+                    <div>
+                        <h3>Sportive</h3>
+                        <p>Cours individuel ou collectif , randonnée organisée</p>
+                    </div>
+                </div>
+                <img src="<?= get_template_directory_uri() . '/assets/img/nos-activites-sportive.jpg' ?>" alt="">
+            </div>
+            <div class="our-activity-live-together__container__card-wrapper__card">
+                <div>
+                    <i class="fa-solid fa-hand"></i>
+                    <div>
+                        <h3>Manuelle</h3>
+                        <p>Cours de cuisine, de poterie, de broderie...</p>
+                    </div>
+                </div>
+                <img src="<?= get_template_directory_uri() . '/assets/img/nos-activites-manuelle.jpeg' ?>" alt="">
+            </div>
+            <div class="our-activity-live-together__container__card-wrapper__card">
+                <div>
+                    <i class="fa-solid fa-landmark"></i>
+                    <div>
+                        <h3>Culturelle</h3>
+                        <p>Voyages, visite de musée, sortie cinéma.</p>
+                    </div>
+                </div>
+                <img src="<?= get_template_directory_uri() . '/assets/img/nos-activites-culturelle.jpeg' ?>" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="our-activity-slogan" class="our-activity-slogan">
+    <div class="our-activity-slogan__container">
+        <div>
+            <div>
+                <h2>Colive'in s'occupe de tout !</h2>
+                <p>Tout au long de l’année vous pourrez profiter d’une multitude d’activités organisées par nos soins.</p>
+            </div>
+            <div>
+                <h2>Nous sommes à votre écoute !</h2>
+                <p>Faites nous part de vos envies même les plus folles et nous trouveront le moyen de les réaliser.</p>
+            </div>
+        </div>
+        <p>Être ensemble, partager, échanger, voyager…</p>
+        <p>C’est ça l’état d’esprit de la communauté Colive’in.</p>
+    </div>
+</section>
+
+<?php get_footer(); ?>
