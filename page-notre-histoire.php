6ce430f6ac5b983c5090b2ac737367d6d234f7f8
Benjamin Picquet <benjamin.picquet@hotmail.com>
2023-06-27 11:19:50 +0200

    add story page integration and responsive

---
+++ page-notre-histoire.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+
+<section id="our-story-subheader" class="our-story-subheader">
+    <div class="our-story-subheader__container container">
+        <div class="our-story-subheader__container__founders">
+            <div>
+                <img src="<?= get_template_directory_uri() . '/assets/img/enzo.jpeg' ?>" alt="">
+                <h3>Enzo</h3>
+            </div>
+            <div>
+                <img src="<?= get_template_directory_uri() . '/assets/img/tony.jpeg' ?>" alt="">
+                <h3>Tony</h3>
+            </div>
+        </div>
+        <div class="our-story-subheader__container__presentation">
+            <h1>Notre histoire</h1>
+            <p>« Après des années dans le monde du salariat dans le secteur de la santé humaine puis animale. Nous avions à cœur de mener un projet entrepreneurial où les valeurs humaines et sociales sont au centre de notre activité.</p>
+            <p>Ce qui nous anime au quotidien et qui fait sens pour nous, c’est d’offrir la possibilité à des milliers de séniors de vivre leur retraite en communauté. Nous allions répondre à un des enjeux majeur de notre société : la solitude de nos aînés.</p>
+            <p>Nous sommes convaincus qu’intégrer notre concept c’est faire le choix de rester jeune et dynamique ! Il y a un proverbe qui dit « on commence à vieillir quand on finit d’apprendre » chez Colive’in on apprend les uns des autres, on crée du partage autour des activités proposées, on réapprend à découvrir le monde grâce aux séjours organisés.</p>
+            <p><strong>Notre engagement </strong>: rendre la colocation pour séniors autonomes accessible et faire de votre retraite une seconde jeunesse ! »</p>
+        </div>
+    </div>
+</section>
+
+<section id="our-story-is-born" class="our-story-is-born">
+    <div class="our-story-is-born__container container">
+        <h2>Et c’est ainsi que l’idée de Colive’in est née !</h2>
+        <p>Vous l’aurez compris, Colive’in est une <strong>entreprise familiale</strong>, et c’est pourquoi nous sommes très attachés à <strong>l’ambiance humaine et conviviale</strong> que nous créons à travers ce projet.</p>
+        <p>Nous nous amusons d’ailleurs très souvent à dire que l’on se considère un peu comme <strong>les petits-enfants</strong> de nos colocataires.</p>
+        <p>Notre idée : vous apporter toutes nos compétences, toute notre énergie, et notre bonne humeur.</p>
+    </div>
+</section>
+
+<section id="our-story-live" class="our-story-live">
+    <div class="our-story-live__container container">
+        <img src="<?= get_template_directory_uri() . '/assets/img/colivein-vivre-ensemble.png' ?>" alt="">
+    </div>
+</section>
+
+<?php get_footer(); ?>
