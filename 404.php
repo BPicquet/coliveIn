6cecce9f6ad1ac323c02ea456265970c6b709bf7
Benjamin Picquet <benjamin.picquet@hotmail.com>
2023-06-27 21:16:21 +0200

    add 404 page: integration and responsive

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<section id="error-404" class="error-404">
+    <div class="error-404__container container">
+        <h1>La page ne peut pas être trouvée.</h1>
+        <p>Il semble que rien n’a été trouvé à cet emplacement.</p>
+	</div>
+</section>
+
+<?php get_footer(); ?>
